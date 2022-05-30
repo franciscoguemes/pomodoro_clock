fd3672a694a3e6cdd455330ac99bb5fb8391dac6
franciscoguemes <francisco.jose.guemes.sevilla@gmail.com>
2022-05-30 16:27:55 +0200

    First steps

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pomodoro_clock
-A pomodoro clock to keep track of you hard work
+A pomodoro clock to keep track of you hard work.
+It is based on the Youtube video of [NeuralNine](https://www.youtube.com/watch?v=FJeXp5yZd-g).
--- main.py
+++ main.py
@@ -10,6 +10,9 @@ class PomodoroTimer:
         self.root = tk.Tk()
         self.root.geometry("600x300")
         self.root.title("Pomodoro, Timer Francisco")
+        # TODO: Image not loading. .jpg files are not loaded by default in tkinter
+        #       you need to use PIL --> https://stackoverflow.com/a/48492788/1866109
+        # self.root.iconphoto(False, PhotoImage(file="Baeldung.png"))
         self.root.tk.call('wm', 'iconphoto', self.root._w, PhotoImage(file="tomato.png"))
 
         self.root.mainloop()
